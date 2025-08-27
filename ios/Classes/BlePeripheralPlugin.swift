e100474a16ab7a501c9272f97a68f8897da4a41f
Ammar Ishfaq <ammarishfaq25@gmail.com>
2025-08-27 16:13:03 +0500

    - ios side transfer of message from and to working now

--- ios/Classes/BlePeripheralPlugin.swift
+++ ios/Classes/BlePeripheralPlugin.swift
@@ -2,107 +2,285 @@ import Flutter
 import UIKit
 import CoreBluetooth
 
-public class BlePeripheralPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, CBPeripheralManagerDelegate {
-    var eventSink: FlutterEventSink?
-    var peripheralManager: CBPeripheralManager?
-    var characteristics = [CBMutableCharacteristic]()
+public class BlePeripheralPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
 
+    // Channels
+    private var methodChannel: FlutterMethodChannel!
+    private var eventChannel: FlutterEventChannel!
+    private var eventSink: FlutterEventSink?
+
+    // Peripheral (Server)
+    private var peripheralManager: CBPeripheralManager!
+    private var txCharacteristic: CBMutableCharacteristic?
+    private var rxCharacteristic: CBMutableCharacteristic?
+    private var serviceUUID: CBUUID!
+    private var txUUID: CBUUID!
+    private var rxUUID: CBUUID!
+    private var subscribers = Set<CBCentral>()
+
+    // Central (Client)
+    private var centralManager: CBCentralManager!
+    private var discoveredPeripherals = [CBPeripheral]()
+    private var connectedPeripherals = [CBPeripheral]()
+    private var peripheralRX: CBCharacteristic?
+
+    // Pending actions
+    private var pendingPeripheralSetup: (service: String, tx: String, rx: String)?
+    private var pendingScanUUID: CBUUID?
+
+    // Flutter Plugin registration
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "ble_peripheral_plugin", binaryMessenger: registrar.messenger())
         let instance = BlePeripheralPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-
-        let eventChannel = FlutterEventChannel(name: "ble_peripheral_plugin/events", binaryMessenger: registrar.messenger())
-        eventChannel.setStreamHandler(instance)
+        instance.methodChannel = FlutterMethodChannel(name: "ble_peripheral_plugin/methods", binaryMessenger: registrar.messenger())
+        instance.eventChannel = FlutterEventChannel(name: "ble_peripheral_plugin/events", binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: instance.methodChannel)
+        instance.eventChannel.setStreamHandler(instance)
     }
 
+    // MARK: - Stream Handler
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil
+    }
+
+    // MARK: - Method Call
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
-        case "startAdvertising":
-            if let args = call.arguments as? [String: Any],
-               let serviceUuid = args["serviceUuid"] as? String,
-               let charUuids = args["characteristics"] as? [String] {
-                startAdvertising(serviceUuid: serviceUuid, charUuids: charUuids)
-            }
+        case "startPeripheral":
+            guard let args = call.arguments as? [String: String],
+                  let service = args["serviceUuid"],
+                  let tx = args["txUuid"],
+                  let rx = args["rxUuid"] else { return }
+            startPeripheral(service: service, tx: tx, rx: rx)
             result(nil)
-        case "stopAdvertising":
-            peripheralManager?.stopAdvertising()
+
+        case "stopPeripheral":
+            stopPeripheral()
             result(nil)
+
         case "sendNotification":
-            if let args = call.arguments as? [String: Any],
-               let charUuid = args["characteristic"] as? String,
-               let value = args["value"] as? FlutterStandardTypedData {
-                sendNotification(charUuid: charUuid, value: value.data)
-            }
+            guard let args = call.arguments as? [String: Any],
+                  let charUuid = args["charUuid"] as? String,
+                  let value = args["value"] as? FlutterStandardTypedData else { return }
+            sendNotification(charUuid: charUuid, value: value.data)
             result(nil)
+
+        case "startScan":
+            guard let args = call.arguments as? [String: String],
+                  let service = args["serviceUuid"] else { return }
+            startScan(service: service)
+            result(nil)
+
+        case "stopScan":
+            stopScan()
+            result(nil)
+
+        case "connect":
+            guard let args = call.arguments as? [String: String],
+                  let deviceId = args["deviceId"] else { return }
+            connect(deviceId: deviceId)
+            result(nil)
+
+        case "disconnect":
+            disconnectAll()
+            result(nil)
+
+        case "writeCharacteristic":
+            guard let args = call.arguments as? [String: Any],
+                  let charUuid = args["charUuid"] as? String,
+                  let value = args["value"] as? FlutterStandardTypedData else { return }
+            writeCharacteristic(charUuid: charUuid, value: value.data)
+            result(nil)
+
         default:
             result(FlutterMethodNotImplemented)
         }
     }
 
-    private func startAdvertising(serviceUuid: String, charUuids: [String]) {
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-
-        let serviceUUID = CBUUID(string: serviceUuid)
-        let service = CBMutableService(type: serviceUUID, primary: true)
-
-        characteristics.removeAll()
-        for uuid in charUuids {
-            let char = CBMutableCharacteristic(
-                type: CBUUID(string: uuid),
-                properties: [.read, .write, .notify],
-                value: nil,
-                permissions: [.readable, .writeable]
-            )
-            characteristics.append(char)
+    // MARK: - Peripheral
+    private func startPeripheral(service: String, tx: String, rx: String) {
+        if peripheralManager?.state == .poweredOn {
+            setupPeripheral(service: service, tx: tx, rx: rx)
+        } else {
+            pendingPeripheralSetup = (service, tx, rx)
+            peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         }
-        service.characteristics = characteristics
-        peripheralManager?.add(service)
+    }
 
-        peripheralManager?.startAdvertising([
-            CBAdvertisementDataServiceUUIDsKey: [serviceUUID],
-            CBAdvertisementDataLocalNameKey: "BLE-Peripheral"
-        ])
+    private func setupPeripheral(service: String, tx: String, rx: String) {
+        serviceUUID = CBUUID(string: service)
+        txUUID = CBUUID(string: tx)
+        rxUUID = CBUUID(string: rx)
+
+        txCharacteristic = CBMutableCharacteristic(
+            type: txUUID,
+            properties: [.notify],
+            value: nil,
+            permissions: [.readable]
+        )
+
+        rxCharacteristic = CBMutableCharacteristic(
+            type: rxUUID,
+            properties: [.write],
+            value: nil,
+            permissions: [.writeable]
+        )
+
+        let service = CBMutableService(type: serviceUUID, primary: true)
+        service.characteristics = [txCharacteristic!, rxCharacteristic!]
+
+        peripheralManager.add(service)
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [serviceUUID!]])
+        sendEvent(["type": "peripheral_started"])
+    }
+
+    private func stopPeripheral() {
+        peripheralManager?.stopAdvertising()
+        peripheralManager = nil
+        subscribers.removeAll()
+        sendEvent(["type": "peripheral_stopped"])
     }
 
     private func sendNotification(charUuid: String, value: Data) {
-        if let char = characteristics.first(where: { $0.uuid.uuidString == charUuid }) {
-            peripheralManager?.updateValue(value, for: char, onSubscribedCentrals: nil)
-        }
+        guard let characteristic = txCharacteristic,
+              charUuid.uppercased() == txUUID.uuidString else { return }
+        peripheralManager.updateValue(value, for: characteristic, onSubscribedCentrals: Array(subscribers))
     }
 
-    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        if peripheral.state == .poweredOn {
-            eventSink?(["event": "powered_on"])
+    // MARK: - Central
+    private func startScan(service: String) {
+        let uuid = CBUUID(string: service)
+        if centralManager?.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [uuid], options: nil)
+            sendEvent(["type": "scan_started"])
         } else {
-            eventSink?(["event": "state_changed", "state": peripheral.state.rawValue])
+            pendingScanUUID = uuid
+            centralManager = CBCentralManager(delegate: self, queue: nil)
         }
     }
 
-    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        if let char = characteristics.first(where: { $0.uuid == request.characteristic.uuid }) {
-            request.value = char.value
-            peripheral.respond(to: request, withResult: .success)
-            eventSink?(["event": "read", "char": char.uuid.uuidString])
+    private func stopScan() {
+        centralManager?.stopScan()
+        sendEvent(["type": "scan_stopped"])
+    }
+
+    private func connect(deviceId: String) {
+        if let peripheral = discoveredPeripherals.first(where: { $0.identifier.uuidString == deviceId }) {
+            centralManager.connect(peripheral, options: nil)
         }
+    }
+
+    private func disconnectAll() {
+        connectedPeripherals.forEach { centralManager.cancelPeripheralConnection($0) }
+        connectedPeripherals.removeAll()
+        sendEvent(["type": "disconnected"])
+    }
+
+    private func writeCharacteristic(charUuid: String, value: Data) {
+        for peripheral in connectedPeripherals {
+            if let rx = peripheralRX, rx.uuid.uuidString.uppercased() == charUuid.uppercased() {
+                peripheral.writeValue(value, for: rx, type: .withResponse)
+            }
+        }
+    }
+
+    // MARK: - Event sending
+    private func sendEvent(_ payload: [String: Any]) {
+        DispatchQueue.main.async {
+            self.eventSink?(payload)
+        }
+    }
+}
+
+// MARK: - CBPeripheralManagerDelegate
+extension BlePeripheralPlugin: CBPeripheralManagerDelegate {
+    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn, let pending = pendingPeripheralSetup {
+            setupPeripheral(service: pending.service, tx: pending.tx, rx: pending.rx)
+            pendingPeripheralSetup = nil
+        } else if peripheral.state != .poweredOn {
+            sendEvent(["type": "peripheral_error", "message": "Bluetooth is not powered on"])
+        }
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        subscribers.insert(central)
+        sendEvent(["type": "server_connected", "deviceId": central.identifier.uuidString])
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        subscribers.remove(central)
+        sendEvent(["type": "server_disconnected", "deviceId": central.identifier.uuidString])
     }
 
     public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
-        for request in requests {
-            if let value = request.value {
-                eventSink?(["event": "write", "char": request.characteristic.uuid.uuidString, "value": [UInt8](value)])
+        for req in requests {
+            if let value = req.value {
+                sendEvent(["type": "rx", "value": value, "deviceId": req.central.identifier.uuidString])
             }
-            peripheral.respond(to: request, withResult: .success)
+            peripheral.respond(to: req, withResult: .success)
+        }
+    }
+}
+
+// MARK: - CBCentralManagerDelegate & CBPeripheralDelegate
+extension BlePeripheralPlugin: CBCentralManagerDelegate, CBPeripheralDelegate {
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn, let uuid = pendingScanUUID {
+            centralManager.scanForPeripherals(withServices: [uuid], options: nil)
+            sendEvent(["type": "scan_started"])
+            pendingScanUUID = nil
+        } else if central.state != .poweredOn {
+            sendEvent(["type": "scan_error", "message": "Bluetooth not powered on"])
         }
     }
 
-    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        eventSink = events
-        return nil
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                               advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if !discoveredPeripherals.contains(peripheral) {
+            discoveredPeripherals.append(peripheral)
+        }
+        sendEvent(["type": "scanResult", "deviceId": peripheral.identifier.uuidString, "name": peripheral.name ?? ""])
     }
 
-    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        eventSink = nil
-        return nil
+    public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices([serviceUUID])
+        connectedPeripherals.append(peripheral)
+        sendEvent(["type": "connected", "deviceId": peripheral.identifier.uuidString])
+    }
+
+    public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        connectedPeripherals.removeAll { $0 == peripheral }
+        sendEvent(["type": "disconnected", "deviceId": peripheral.identifier.uuidString])
+    }
+
+    public func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics([rxUUID, txUUID], for: service)
+        }
+    }
+
+    public func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let chars = service.characteristics else { return }
+        for char in chars {
+            if char.uuid == txUUID {
+                peripheral.setNotifyValue(true, for: char)
+            }
+            if char.uuid == rxUUID {
+                peripheralRX = char
+            }
+        }
+    }
+
+    public func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        guard let value = characteristic.value else { return }
+        sendEvent(["type": "notification", "value": value, "deviceId": peripheral.identifier.uuidString])
     }
 }
